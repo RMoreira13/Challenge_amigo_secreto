40f803f41cbcc91471c5cf12aa938158d9fbca76
Rafael Moreira <rafael.castro13@hotmail.com>
2025-02-01 20:03:40 +0100

    Criado uma Function para capturar o Value digitado no input.

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+let amigos_participantes = [];
 
 
+/* FUNÇÃO CAPTURAR NOME DO AMIGO */
+
+function adicionarAmigo() {
+    let inputNome = document.getElementById("amigo");
+    let NomeAmigo = inputNome.value.trim(); // Remove espaços extras
+    console.log(NomeAmigo);
+}
