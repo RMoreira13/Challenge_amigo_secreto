a48910490453f21596f8974e7ac2fe7aeb267e3e
Rafael Moreira <rafael.castro13@hotmail.com>
2025-02-01 20:45:04 +0100

    Criado uma Function para atualizar a lista de nomes e exibir os nomes em uma lista visível na tela.

--- app.js
+++ app.js
@@ -11,8 +11,22 @@ function adicionarAmigo() {
     
     if (NomeAmigo != amigos_participantes) {
         amigos_participantes.push(NomeAmigo); // Adiciona ao array
+        atualizarLista(); // Atualiza a exibição
         inputNome.value = ""; // Limpa o inputNome
     } else {
         alert("Este nome já está na lista, por favor digite outro nome!");
     }
+}
+
+/* FUNÇÃO ATUALIZAR LISTA DE NOMES DOS AMIGOS PARTICIPANTES */
+
+function atualizarLista() {
+    let listaNomes = document.getElementById("listaAmigos");
+    listaNomes.innerHTML = ""; // Limpa a lista antes de atualizar
+    
+    amigos_participantes.forEach((item, index) => {
+        let li = document.createElement("li");
+        li.textContent = item;
+        listaNomes.appendChild(li);
+    });
 }
