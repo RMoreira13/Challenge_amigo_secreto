a991482b9c643de297628e60f844904589e6d447
Rafael Moreira <rafael.castro13@hotmail.com>
2025-02-02 17:59:11 +0100

    Criado condição onde não pode haver o campo nome em branco e correção da condição de verificação de nomes da lista

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos_participantes = [];
+console.log(amigos_participantes);
 
 
 /* FUNÇÃO CAPTURAR NOME DO AMIGO */
 
 function adicionarAmigo() {
-    let inputNome = document.getElementById("amigo");
-    let NomeAmigo = inputNome.value.trim(); // Remove espaços extras
+
+    let inputNome = document.getElementById("amigo").value.trim(); // Remove espaços extras;
     
-    if (NomeAmigo != amigos_participantes) {
-        amigos_participantes.push(NomeAmigo); // Adiciona ao array
+    if (inputNome == ""){
+        alert("Digite um nome, o campo não pode estar vazio!")
+    } else if (amigos_participantes.includes(inputNome)) {
+        alert("Este nome já está na lista, por favor digite outro nome!");
+    } else {
+        amigos_participantes.push(inputNome); // Adiciona ao array
         atualizarLista(); // Atualiza a exibição
         inputNome.value = ""; // Limpa o inputNome
-    } else {
-        alert("Este nome já está na lista, por favor digite outro nome!");
     }
 }
 
